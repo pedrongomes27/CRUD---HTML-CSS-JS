382dbb97558bda37435fd69ae6fe58f169ec1747
pedrongomes27 <pedro.ngomes27@hotmail.com>
2023-02-26 16:55:14 -0300

    Comentarios no Script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const list = document.getElementById('list');
 // Obtém a referência ao botão HTML com o ID "add-button"
 const addButton = document.getElementById('add-button');
+// Obtém a referência ao botão HTML com o ID "save-button"
 const saveButton = document.getElementById('save-button');
 
 
@@ -35,6 +36,7 @@ addButton.addEventListener('click', () => {
         document.getElementById('priority').value = '';
         document.getElementById('due-date').value = '';
 
+        // Dá reload na página
         location.reload();
     } else {
         // Exibe uma mensagem de erro ou destaca o campo de título para o usuário
@@ -58,6 +60,8 @@ saveButton.addEventListener('click', () => {
             priority,
             dueDate
         };
+
+        // Define o item atualizado na posição do índice especificado na lista de tarefas.
         todoList[currentIndex] = updatedItem;
 
         // Salva a lista atualizada no armazenamento local do navegador
@@ -74,6 +78,7 @@ saveButton.addEventListener('click', () => {
         // Renderiza a lista atualizada
         renderList();
 
+        // Dá reload na página
         location.reload();
     } else {
         // Exibe uma mensagem de erro ou destaca o campo de título para o usuário
@@ -89,9 +94,12 @@ function addItem(item) {
 }
 
 function editItem(index) {
-    window.scrollTo(0,0);
+    // Faz com que a janela do navegador role para o topo da página
+    window.scrollTo(0, 0);
 
+    // Obtém o item da lista de tarefas com o índice especificado e define os valores dos campos do formulário com as informações desse item
     const item = todoList[index];
+    // O formulário é preenchido com os valores de cada campo
     document.getElementById('title').value = item.title;
     document.getElementById('description').value = item.description;
     document.getElementById('priority').value = item.priority;
@@ -121,6 +129,7 @@ function renderList() {
         const div = document.createElement('div');
         div.classList.add('div-js');
 
+        // Cria um elemento de input do tipo checkbox
         const checkbox = document.createElement('input');
         checkbox.classList.add('task-checkboxas');
         checkbox.type = 'checkbox';
@@ -136,32 +145,37 @@ function renderList() {
         const checked = localStorage.getItem(`todoItem-${index}-checked`);
         checkbox.checked = checked === 'true';
 
+        // Cria um elemento de título da tarefa e define seu valor como o título da tarefa atual
         const h2 = document.createElement('h2');
         h2.classList.add('task-title');
         h2.innerText = item.title;
 
+        // Cria um elemento de data de vencimento da tarefa e define seu valor como a data de vencimento da tarefa atual
         const pDueDate = document.createElement('p');
         pDueDate.innerText = item.dueDate;
         pDueDate.classList.add('task-due-date');
 
+        // Cria um elemento de prioridade da tarefa e define sua classe como a prioridade da tarefa atual
         const pPriority = document.createElement('p');
-        // pPriority.innerText = item.priority;
         pPriority.classList.add('task-priority');
         pPriority.classList.add(`priority-${item.priority}`);
-        
 
+        // Adiciona os elementos criados anteriormente ao elemento div
         div.appendChild(checkbox);
         div.appendChild(h2);
         div.appendChild(pDueDate);
         div.appendChild(pPriority);
 
+        // Cria um elemento de descrição da tarefa e define seu valor como a descrição da tarefa atual
         const pDescription = document.createElement('p');
         pDescription.innerText = item.description;
         pDescription.classList.add('task-description')
 
+        // Cria um elemento de descrição da tarefa e define seu valor como a descrição da tarefa atual
         const divButton = document.createElement('div');
         divButton.classList.add('div-js');
 
+        // Cria um botão de exclusão da tarefa
         const deleteButton = document.createElement('button');
         deleteButton.classList.add('option-button');
         deleteButton.id = 'deleteButton';
@@ -169,11 +183,12 @@ function renderList() {
         const deleteText = document.createElement('span');
         deleteText.innerText = 'Deletar';
 
+        // Adiciona o ícone e o texto ao botão de exclusão e adiciona um ouvinte de eventos de clique
         deleteButton.appendChild(deleteIcon);
         deleteButton.appendChild(deleteText);
         deleteButton.addEventListener('click', () => deleteItem(index));
 
-
+        // Cria um botão de edição da tarefa
         const editButton = document.createElement('button');
         editButton.classList.add('option-button');
         editButton.id = 'editButton';
@@ -181,12 +196,14 @@ function renderList() {
         const editText = document.createElement('span');
         editText.innerText = 'Editar';
 
+        // Adiciona o ícone e o texto ao botão de edição
         editButton.appendChild(editIcon);
         editButton.appendChild(editText);
 
         // Adiciona um ouvinte de eventos de clique ao botão "Edit"
         editButton.addEventListener('click', () => editItem(index));
 
+        // Adiciona os botões edit e delete a div
         divButton.appendChild(editButton);
         divButton.appendChild(deleteButton);
 
@@ -197,7 +214,7 @@ function renderList() {
         li.appendChild(pDescription);
         li.appendChild(divButton);
 
-        
+
         // Adiciona o elemento HTML <li> à lista de tarefas
         list.appendChild(li);
     });
@@ -211,8 +228,10 @@ function saveList() {
 
 // Carregar a lista de tarefas salva no armazenamento local do navegador, se houver
 function loadList() {
+    // Obtém a lista de tarefas salva no armazenamento local
     const savedList = localStorage.getItem('todoList');
 
+    // Se houver uma lista salva, converte o JSON de volta para um objeto e renderiza a lista na página
     if (savedList !== null) {
         todoList = JSON.parse(savedList);
         renderList();
@@ -221,5 +240,7 @@ function loadList() {
 
 // Inicializar a lista de tarefas vazia e carregar a lista salva, se houver
 let todoList = [];
-let currentIndex = -1; // inicialmente, nenhum item está sendo editado
+// Inicialmente, nenhum item está sendo editado
+let currentIndex = -1; 
+
 loadList();
